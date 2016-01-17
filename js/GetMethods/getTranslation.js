0ef97d2a37f4aa778cf1f7e50bbc50a0cbeb75c5
JustinHoyt <justinhoyt24@gmail.com>
2016-01-16 23:14:33 -0500

    hooked up http request with parse.js

--- js/GetMethods/getTranslation.js
+++ js/GetMethods/getTranslation.js
@@ -1,16 +1,26 @@
+var language = "es";
+
 //Callback function
 function translateText(response) {
-    var translatedText = response.data.translations[0].translatedText;
-    $("#translation").text(translatedText);
+    var translatedPhrase = response.data.translations[0].translatedText;
+    return translatedPhrase;
 }
 
-$("#language").change(function() {
+
+function translate(word) {
     var $language = $("#language");
     var $sourceText = $("#sourceText");
     var url = 'https://www.googleapis.com/language/translate/v2?key=AIzaSyCL9jeVEeDwzPxHXkDv39woblcUc538pDM&source=en' +
-        '&target=' + $language.val() +
-        '&q=' + $sourceText.text();
+        '&target=' + language +
+        '&q=' + word;
 
     //GET method
-    $.get(url, translateText);
+    var translatedPhrase = $.get(url, translateText);
+    return translatedPhrase;
+};
+
+
+//ignore for now
+$("#language").change(function() {
+    language = $("#language");
 });
--- js/parse.js
+++ js/parse.js
@@ -1,2 +1,16 @@
-var $paragraph = $("p");
-$paragraph.html("HODOR");
+
+//selects the first paragraph tag
+var n = 5;
+var $p = $("p:eq(0)");
+var pString = $p.text();
+pString = pString.split(" ");
+
+var word = pString[n];
+
+var translatedPhrase = translate(word);
+
+pString[n] = translatedPhrase;
+
+pString = pString.join(" ");
+
+$p.html(pString);
