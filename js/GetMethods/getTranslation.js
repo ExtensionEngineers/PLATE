7e28b68a1d6f1574431120919865ebedca699d6c
JustinHoyt <justinhoyt24@gmail.com>
2016-01-17 18:10:09 -0500

    deleted parse.js and moved functionality to getTranslation.js

--- js/GetMethods/getTranslation.js
+++ js/GetMethods/getTranslation.js
@@ -1,36 +1,30 @@
 var language = "es";
-var index = 0;
+var paragraphIndex;
+var $p = $("p:eq(0)");
+var pString = $p.text();
 
-//Callback function
-function translateText(response) {
-    var $p = $("p:eq(0)");
+pString = pString.split(" ");
+paragraphIndex = Math.floor((Math.random() * pString.length) + 1);
+var word = pString[paragraphIndex];
+
+var $language = $("#language");
+var $sourceText = $("#sourceText");
+var url = 'https://www.googleapis.com/language/translate/v2?key=AIzaSyCL9jeVEeDwzPxHXkDv39woblcUc538pDM&source=en' +
+          '&target=' + language +
+          '&q=' + word;
+
+//GET method
+$.get(url,function(response) {
     var translatedWord = response.data.translations[0].translatedText;
-
     var pString = $p.text();
     pString = pString.split(" ");
-
-    pString[index] = "<strong>" + translatedWord + "</strong>";
-
+    pString[paragraphIndex] = "<strong>" + translatedWord + "</strong>";
     pString = pString.join(" ");
+
     $p.html(pString);
-}
+});
 
-
-function translate(word, n) {
-    index = n;
-    var $language = $("#language");
-    var $sourceText = $("#sourceText");
-    var url = 'https://www.googleapis.com/language/translate/v2?key=AIzaSyCL9jeVEeDwzPxHXkDv39woblcUc538pDM&source=en' +
-        '&target=' + language +
-        '&q=' + word;
-
-    //GET method
-    $.get(url, translateText);
-};
-
-
-
-//ignore for now
+//not yet implemented
 $("#language").change(function() {
     language = $("#language");
 });
