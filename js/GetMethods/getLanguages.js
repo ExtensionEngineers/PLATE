8adf6d2db04754a533b655145d5a7749615ae838
JustinHoyt <justinhoyt24@gmail.com>
2016-01-23 12:14:17 -0500

    The dropdown now changes the langauge to be translated (beta stage)

--- js/GetMethods/getLanguages.js
+++ js/GetMethods/getLanguages.js
@@ -1,3 +1,4 @@
+var $language = $("#language");
 //Callback function
 function injectLanguages(response){
     var dropdownHTML = "";
@@ -15,3 +16,7 @@ function injectLanguages(response){
 var url = "https://www.googleapis.com/language/translate/v2/languages?key=AIzaSyCL9jeVEeDwzPxHXkDv39woblcUc538pDM&target=en";
 //GET method
 $.get(url, injectLanguages);
+
+$("#language").change(function() {
+    chrome.storage.sync.set({language: $language.val()});
+});
--- js/GetMethods/getTranslation.js
+++ js/GetMethods/getTranslation.js
@@ -1,30 +1,32 @@
-var language = "es";
-var paragraphIndex;
-var $p = $("p:eq(0)");
-var pString = $p.text();
 
-pString = pString.split(" ");
-paragraphIndex = Math.floor((Math.random() * pString.length) + 1);
-var word = pString[paragraphIndex];
 
-var $language = $("#language");
-var $sourceText = $("#sourceText");
-var url = 'https://www.googleapis.com/language/translate/v2?key=AIzaSyCL9jeVEeDwzPxHXkDv39woblcUc538pDM&source=en' +
-          '&target=' + language +
-          '&q=' + word;
-
-//GET method
-$.get(url,function(response) {
-    var translatedWord = response.data.translations[0].translatedText;
+chrome.storage.onChanged.addListener(function() {
+    var language;
+    var paragraphIndex;
+    var $p = $("p:eq(0)");
     var pString = $p.text();
-    pString = pString.split(" ");
-    pString[paragraphIndex] = "<strong>" + translatedWord + "</strong>";
-    pString = pString.join(" ");
+    chrome.storage.sync.get(null, function(data) {
+        language = data.language;
 
-    $p.html(pString);
-});
+        pString = pString.split(" ");
+        paragraphIndex = Math.floor((Math.random() * pString.length) + 1);
+        var word = pString[paragraphIndex];
 
-//not yet implemented
-$("#language").change(function() {
-    language = $("#language");
+        var $language = $("#language");
+        var $sourceText = $("#sourceText");
+        var url = 'https://www.googleapis.com/language/translate/v2?key=AIzaSyCL9jeVEeDwzPxHXkDv39woblcUc538pDM&source=en' +
+            '&target=' + language +
+            '&q=' + word;
+
+        //GET method
+        $.get(url,function(response) {
+            var translatedWord = response.data.translations[0].translatedText;
+            var pString = $p.text();
+            pString = pString.split(" ");
+            pString[paragraphIndex] = "<strong>" + translatedWord + "</strong>";
+            pString = pString.join(" ");
+
+            $p.html(pString);
+        });
+    });
 });
