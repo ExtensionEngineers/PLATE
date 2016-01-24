f02ea7bc1796e69d769383ec70df4f8ca9be0b0e
JustinHoyt <justinhoyt24@gmail.com>
2016-01-24 17:07:45 -0500

    enabled button works with extension

--- js/GetMethods/getTranslation.js
+++ js/GetMethods/getTranslation.js
@@ -1,12 +1,45 @@
-function translate(){
-    var paragraphIndex;
-    var apiKey = "AIzaSyCL9jeVEeDwzPxHXkDv39woblcUc538pDM";
-    var apiSource = "en";
-    var $p = $("p:eq(0)");
-    var pString = $p.text();
-    chrome.storage.sync.get(null, function(data) {
-        var language = data.language;
+chrome.storage.onChanged.addListener(translate);
 
+translate();
+
+function translate(){
+    chrome.storage.sync.get(null, function(data){
+        if(data.language == null){
+            getTranslatedWord();
+        }
+        if (data.enabled == true){
+            getTranslatedWord();
+        }
+    });
+}
+
+
+function getTranslatedWord(){
+    chrome.storage.sync.get(null, function(data) {
+        if (data.language == null){
+            chrome.storage.sync.set({
+                language: "es",
+                name: "Spanish",
+                enabled: true
+            });
+            //chrome.storage.sync.set({name: "Spanish"});
+            //chrome.storage.sync.set({enabled: true});
+            findReplace();
+        }
+        else{
+            findReplace();
+        }
+    });
+}
+
+function findReplace(){
+    chrome.storage.sync.get(null, function(data){
+        var language = data.language;
+        var paragraphIndex;
+        var apiKey = "AIzaSyCL9jeVEeDwzPxHXkDv39woblcUc538pDM";
+        var apiSource = "en";
+        var $p = $("p:eq(0)");
+        var pString = $p.text();
         pString = pString.split(" ");
         paragraphIndex = Math.floor((Math.random() * pString.length) + 1);
         var word = pString[paragraphIndex];
@@ -29,8 +62,3 @@ function translate(){
         });
     });
 }
-
-translate();
-
-chrome.storage.onChanged.addListener(translate);
-
---
+++ js/GetMethods/settingsPopup.js
@@ -0,0 +1,62 @@
+var $language = $("#language");
+var $enableButton = $("#enableButton");
+var url = "https://www.googleapis.com/language/translate/v2/languages?key=AIzaSyCL9jeVEeDwzPxHXkDv39woblcUc538pDM&target=en";
+$.get(url, injectLanguages);
+
+//restores button the storage's state
+chrome.storage.sync.get(null, function(data){
+    if (data.enabled == true){
+        $enableButton.text("On");
+    }
+    else if (data.enabled == false) {
+        $enableButton.text("Off");
+    }
+    //else{  //this runs when the storage is uninitalized on install
+    //    chrome.storage.sync.set({enabled: true});
+    //}
+});
+
+$language.change(function() {
+    chrome.storage.sync.set({
+        name: $language.find(":selected").text(),
+        language: $language.val()
+    });
+    //chrome.storage.sync.set({language: $language.val()});
+});
+
+$enableButton.click(function(){
+    chrome.storage.sync.get(null, function(data){
+        if ($enableButton.text() == "On"){
+            $enableButton.text("Off");
+            chrome.storage.sync.set({enabled: false});
+            chrome.storage.sync.get(null, function(response){
+                console.log(response.enabled);
+            });
+        }
+        else{
+            $enableButton.text("On");
+            chrome.storage.sync.set({enabled: true});
+            chrome.storage.sync.get(null, function(response){
+                console.log(response.enabled);
+            });
+        }
+    });
+});
+
+//Callback function
+function injectLanguages(response){
+    var dropdownHTML = "";
+    //<option value="fr">
+    //  French
+    //</option>
+    $.each(response.data.languages, function(i, item){
+        dropdownHTML += '<option value="' + item.language + '">';
+        dropdownHTML +=     item.name;
+        dropdownHTML += '</option>';
+    });
+    $('#language').html(dropdownHTML);
+
+    chrome.storage.sync.get(null, function(data){
+        $language.val(data.language);
+    });
+}
