6cc19a0216d2bf180f5d1421e41f8074c8deb820
JustinHoyt <justinhoyt24@gmail.com>
2016-01-25 19:40:37 -0500

    translates one word in each paragraph

--- js/GetMethods/getTranslation.js
+++ js/GetMethods/getTranslation.js
@@ -24,25 +24,27 @@ function findReplace(){
         var paragraphIndex;
         var apiKey = "AIzaSyCL9jeVEeDwzPxHXkDv39woblcUc538pDM";
         var apiSource = "en";
-        var $p = $("p:eq(0)");
-        var pString = $p.text();
-        pString = pString.split(" ");
-        paragraphIndex = Math.floor((Math.random() * pString.length) + 1);
-        var word = pString[paragraphIndex];
 
-        var url = 'https://www.googleapis.com/language/translate/v2' +
-            '?key=' + apiKey +
-            '&source=' + apiSource +
-            '&target=' + language +
-            '&q=' + word;
-        $.get(url,function(response) {
-            var translatedWord = response.data.translations[0].translatedText;
-            var pString = $p.text();
+        $.each($("p"), function(i, item){
+            var $item = $(item);
+            var pString = $item.text();
             pString = pString.split(" ");
-            pString[paragraphIndex] = "<strong>" + translatedWord + "</strong>";
-            pString = pString.join(" ");
+            paragraphIndex = Math.floor((Math.random() * pString.length) + 1);
+            var word = pString[paragraphIndex];
 
-            $p.html(pString);
+            var url = 'https://www.googleapis.com/language/translate/v2' +
+                '?key=' + apiKey +
+                '&source=' + apiSource +
+                '&target=' + language +
+                '&q=' + word;
+
+            $.get(url,function(response) {
+                var translatedWord = response.data.translations[0].translatedText;
+                pString[paragraphIndex] = "<strong>" + translatedWord + "</strong>";
+                pString = pString.join(" ");
+
+                $item.html(pString);
+            });
         });
     });
 }
